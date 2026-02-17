511ece8dda7dc1c41d1d046bc63f5d76f767657a
chldPDms <115327796+chldPDms@users.noreply.github.com>
2026-02-17 18:34:41 +0900

    보일러 플레이팅

---
+++ src/main/java/mars/tripplanappbackend/TripPlanAppBackendApplication.java
@@ -0,0 +1,13 @@
+package mars.tripplanappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlanAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlanAppBackendApplication.class, args);
+	}
+
+}
