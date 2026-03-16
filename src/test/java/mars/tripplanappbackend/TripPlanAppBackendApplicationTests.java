df6d9068bcf8587d43ed159dcd833d68ae30419e
김민혁 <royalkim0707@gmail.com>
2026-03-16 22:57:46 +0900

    패키지 선언 완료(수정)

--- src/test/java/mars/tripplanappbackend/TripPlanAppBackendApplicationTests.java
+++ src/test/java/mars/tripplanappbackend/TripPlanAppBackendApplicationTests.java
@@ -2,8 +2,11 @@ package mars.tripplanappbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import mars.tripplanappbackend.global.config.DotenvTestInitializer;
 
 @SpringBootTest
+@ContextConfiguration(initializers = DotenvTestInitializer.class)
 class TripPlanAppBackendApplicationTests {
 
 	@Test
