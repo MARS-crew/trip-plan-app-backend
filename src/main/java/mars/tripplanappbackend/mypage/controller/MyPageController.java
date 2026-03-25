1990463cb4299a0801a8475d994841b280042d30
chldPDms <115327796+chldPDms@users.noreply.github.com>
2026-03-26 01:38:28 +0900

    feat: 사용자 프로필 조회 기능

--- src/main/java/mars/tripplanappbackend/mypage/controller/MyPageController.java
+++ src/main/java/mars/tripplanappbackend/mypage/controller/MyPageController.java
@@ -1,10 +1,15 @@
 package mars.tripplanappbackend.mypage.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import mars.tripplanappbackend.global.config.swagger.ApiErrorExceptions;
+import mars.tripplanappbackend.global.dto.ApiResponse;
+import mars.tripplanappbackend.global.enums.ErrorCode;
+import mars.tripplanappbackend.mypage.dto.resopnse.MyProfileResponseDto;
 import mars.tripplanappbackend.mypage.service.MyPageService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/mypage")
@@ -13,4 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyPageController {
 
     private final MyPageService myPageService;
+
+    @GetMapping("/me")
+    @ApiErrorExceptions({ErrorCode.FORBIDDEN, ErrorCode.INVALID_TOKEN, ErrorCode.EXPIRED_REFRESH_TOKEN,
+            ErrorCode.INVALID_INPUT, ErrorCode.INTERNAL_ERROR})
+    @Operation(summary = "프로필 조회", description = "프로필 조회 api")
+    public ApiResponse<MyProfileResponseDto> getUserInfo(
+            Authentication authentication
+    ) {
+        String usersId = authentication.getName();
+        MyProfileResponseDto response = myPageService.getMyProfile(usersId);
+        return ApiResponse.ok(response);
+    }
+
 }
--- src/main/java/mars/tripplanappbackend/mypage/service/MyPageService.java
+++ src/main/java/mars/tripplanappbackend/mypage/service/MyPageService.java
@@ -1,6 +1,10 @@
 package mars.tripplanappbackend.mypage.service;
 
 import lombok.RequiredArgsConstructor;
+import mars.tripplanappbackend.global.enums.ErrorCode;
+import mars.tripplanappbackend.global.exception.BusinessException;
+import mars.tripplanappbackend.mypage.domain.User;
+import mars.tripplanappbackend.mypage.dto.resopnse.MyProfileResponseDto;
 import mars.tripplanappbackend.mypage.repository.SavedPlaceRepository;
 import mars.tripplanappbackend.mypage.repository.MyPageRepository;
 import org.springframework.stereotype.Service;
@@ -13,4 +17,22 @@ public class MyPageService {
 
     private final MyPageRepository myPageRepository;
     private final SavedPlaceRepository savedPlaceRepository;
+
+    /**
+     * 현재 로그인한 사용자의 프로필 정보를 조회
+     *
+     *
+     * @param usersId JWT 토큰에서 추출된 사용자 식별자
+     * @return 사용자 프로필 정보가 담긴 response
+     *
+     * 사용자를 찾을 수 없는 경우 USER_NOT_FOUND
+     */
+    @Transactional(readOnly = true)
+    public MyProfileResponseDto getMyProfile(String usersId) {
+
+        User user = myPageRepository.findByUsersId(usersId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        return new MyProfileResponseDto(user);
+    }
 }
