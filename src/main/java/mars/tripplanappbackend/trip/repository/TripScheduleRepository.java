19dec15db04b00c27d19567e23f8d15e6c2a99de
chldPDms <115327796+chldPDms@users.noreply.github.com>
2026-04-11 14:57:51 +0900

    feat: 임시 비밀번호 이메일 전송 구현

--- src/main/java/mars/tripplanappbackend/trip/repository/TripScheduleRepository.java
+++ src/main/java/mars/tripplanappbackend/trip/repository/TripScheduleRepository.java
@@ -1,5 +1,6 @@
 package mars.tripplanappbackend.trip.repository;
 
+import mars.tripplanappbackend.trip.domain.Trip;
 import mars.tripplanappbackend.trip.domain.TripSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
