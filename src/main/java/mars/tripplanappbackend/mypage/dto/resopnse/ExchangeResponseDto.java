c7f970ea8394fa26acdceb85cf95d1130934be4c
chldPDms <115327796+chldPDms@users.noreply.github.com>
2026-04-12 22:26:57 +0900

    fix: 코드 구조 변경

--- src/main/java/mars/tripplanappbackend/mypage/dto/resopnse/ExchangeResponseDto.java
+++ src/main/java/mars/tripplanappbackend/mypage/dto/resopnse/ExchangeResponseDto.java
@@ -9,15 +9,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExchangeResponseDto {
-    @Schema(description = "통화 코드", example = "JPY")
+    @Schema(description = "통화 코드", example = "JPY, 통화 코드")
     private String curUnit;
 
-    @Schema(description = "통화명", example = "일본 엔")
+    @Schema(description = "통화명", example = "일본 엔, 통화명")
     private String curNm;
 
-    @Schema(description = "매매 기준율", example = "0.110000")
+    @Schema(description = "1 KRW 기준 환율", example = "0.110000, 1 KRW 기준 환율")
     private double dealBasR;
 
-    @Schema(description = "변환된 금액", example = "1100.0")
+    @Schema(description = "변환된 금액", example = "1100.0, 변환된 금액")
     private double convertedAmount;
+
+    @Schema(description = "기준 날짜", example = "2024-06-01, 환율 기준 날짜")
+    private String searchDate;
 }
