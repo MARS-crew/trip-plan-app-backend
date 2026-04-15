9ee384ebafa687a92005791211d3b7f45658c2e0
apie0711 <apie0711@naver.com>
2026-04-15 17:58:33 +0900

    feat : PLI-17-6-1(내 여행 날짜 수정 API 개발)

--- src/main/java/mars/tripplanappbackend/trip/dto/request/UpdateTripDateRequestDto.java
+++ src/main/java/mars/tripplanappbackend/trip/dto/request/UpdateTripDateRequestDto.java
@@ -12,9 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class UpdateTripDateRequestDto {
 
-    // 여행 시작 날짜
     private LocalDate startDate;
 
-    // 여행 종료 날짜
     private LocalDate endDate;
 }
